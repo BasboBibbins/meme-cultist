e5535a633e27234ad0310bd7c96432956eae99ed
brendanpayne <payneb2@mail.uc.edu>
2023-02-14 19:21:23 -0500

    removed queue. might add it back later.

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const wait = require('util').promisify(setTimeout);
 const logger = require('../../utils/logger');
 const { queueString } = require('../../utils/musicPlayer');
@@ -11,11 +11,6 @@ module.exports = {
             subcommand
                 .setName("clear")
                 .setDescription("Clear the queue.")
-        )
-        .addSubcommand((subcommand) =>
-            subcommand
-                .setName("loop")
-                .setDescription("Toggle queue looping.")
         )
         .addSubcommand((subcommand) =>
             subcommand
@@ -55,33 +50,98 @@ module.exports = {
         const subcommand = interaction.options.getSubcommand();
         switch (subcommand) {
             case "clear":
-                queue.node.clear();
-                embed.setTitle("Queue cleared!");
-                embed.setDescription(`The queue has been cleared!`);
-                await interaction.editReply({embeds: [embed], ephemeral: true});
+                embed.setTitle(`Are you sure you want to clear the queue?`);
+                embed.setDescription(`This will clear the queue and stop the current song. You can cancel this by clicking the cancel button.`);
+                const row = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId("clearQueue")
+                        .setLabel("Clear Queue")
+                        .setStyle(ButtonStyle.Danger),
+                    new ButtonBuilder()
+                        .setCustomId("cancel")
+                        .setLabel("Cancel")
+                        .setStyle(ButtonStyle.Secondary),
+                );
+                await interaction.editReply({embeds: [embed], components: [row], ephemeral: true});
+                const filter = (i) => i.customId === "clearQueue" || i.customId === "cancel";
+                const collector = interaction.channel.createMessageComponentCollector({ filter, time: 30000 });
+                collector.on("collect", async (i) => {
+                    if (i.customId === "clearQueue") {
+                        queue.node.stop();
+                        tracks.clear();
+                        embed.setTitle(`Queue has been cleared!`);
+                        embed.setDescription(`The queue has been cleared and the current song has been stopped.`);
+                        await i.update({embeds: [embed], components: []});
+                        await wait(30000).then(() => i.deleteReply());
+                    } else if (i.customId === "cancel") {
+                        embed.setTitle(`Queue clear cancelled!`);
+                        embed.setDescription(`The queue clear has been cancelled.`);
+                        await i.update({embeds: [embed], components: []});
+                        await wait(30000).then(() => i.deleteReply());
+                    }
+                });
+                collector.on("end", async (collected, reason) => {
+                    if (reason === "time") {
+                        embed.setTitle(`Queue clear timed out!`);
+                        embed.setDescription(`The queue clear has timed out.`);
+                        await interaction.editReply({embeds: [embed], components: []});
+                        await wait(30000).then(() => interaction.deleteReply());
+                    }
+                });
                 break;
-            case "loop":
-                queue.node.setRepeatMode(queue.repeatMode ? 0 : 1);
-                embed.setTitle(`Queue is ${queue.repeatMode ? "now" : "no longer"} looping!`);
-                embed.setDescription(`Song looping has been ${queue.repeatMode ? "enabled" : "disabled"}! You can ${queue.repeatMode ? "disable" : "enable"} it by using this command again.`);
-                await interaction.editReply({embeds: [embed], ephemeral: true});
-                break;
+
             case "shuffle":
-                tracks.shuffle();
-                const result = queueString(tracks);
-                embed.setAuthor({ name: `${interaction.guild.name} has shuffled the queue!`, iconURL: interaction.guild.iconURL({dynamic: true}) });
-                embed.setTitle(`New queue:`)
-                embed.setDescription(`${result}`);
-                await interaction.editReply({embeds: [embed]});
-                await wait(10000).then(() => interaction.deleteReply());
+                embed.setTitle(`Are you sure you want to shuffle the queue?`);
+                embed.setDescription(`This will shuffle all the songs in the queue. You can cancel this by clicking the cancel button.`);
+                const row2 = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId("shuffleQueue")
+                        .setLabel("Shuffle Queue")
+                        .setStyle(ButtonStyle.Danger),
+                    new ButtonBuilder()
+                        .setCustomId("cancel")
+                        .setLabel("Cancel")
+                        .setStyle(ButtonStyle.Secondary),
+                );
+                await interaction.editReply({embeds: [embed], components: [row2], ephemeral: true});
+                const filter2 = (i) => i.customId === "shuffleQueue" || i.customId === "cancel";
+                const collector2 = interaction.channel.createMessageComponentCollector({ filter2, time: 30000 });
+                collector2.on("collect", async (i) => {
+                    if (i.customId === "shuffleQueue") {
+                        tracks.shuffle();
+                        interaction.deleteReply();
+
+                        embed.setAuthor({ name: `${interaction.user.username} has shuffled the queue!`, iconURL: interaction.user.displayAvatarURL({dynamic: true}) });
+                        embed.setTitle(`New queue:`)
+                        embed.setDescription(`${queueString(tracks)}`);
+                        let msg = await interaction.channel.send({embeds: [embed]});
+                        await wait(30000).then(() => {
+                            msg.delete();
+                        });
+                    } else if (i.customId === "cancel") {
+                        embed.setTitle(`Request cancelled!`);
+                        embed.setDescription(`The request to shuffle the queue has been cancelled.`);
+                        await i.update({embeds: [embed], components: []});
+                        await wait(30000).then(() => i.deleteReply());
+                    }
+                });
+                collector2.on("end", async (collected, reason) => {
+                    if (reason === "time") {
+                        embed.setTitle(`Request timed out!`);
+                        embed.setDescription(`The request to shuffle the queue has timed out.`);
+                        await interaction.editReply({embeds: [embed], components: []});
+                        await wait(30000).then(() => interaction.deleteReply());
+                    }
+                });
                 break;
+
             case "view":
-                embed.setTitle(`Queue for ${interaction.guild.name}`);
-                //embed.setDescription(`Currently playing: [${currentTrack.title}](${currentTrack.url})`);
-                embed.addFields(
-                    { name: "Up Next", value: tracks.map((track, i) => `${i + 1}. [${track.title}](${track.url})`).join("\n") || "No songs in queue." },
-                )
+                embed.setTitle(`Current queue:`)
+                embed.setDescription(`${queueString(tracks)}`);
                 await interaction.editReply({embeds: [embed], ephemeral: true});
+                await wait(30000).then(() => interaction.deleteReply());
                 break;
             default: "view";
         }
