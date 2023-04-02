a6e0c2a6b71943e17177dd1be9aca43619178fd2
brendanpayne <basbobibbins@gmail.com>
2023-04-02 11:32:53 -0400

    craps command disabled (for now)

--- commands/currency/craps.js
+++ commands/currency/craps.js
@@ -8,6 +8,7 @@ const { roll, drawDice } = require('../../utils/roll');
 const wait = require('node:timers/promises').setTimeout;
 const logger = require("../../utils/logger");
 const { randomHexColor } = require('../../utils/randomcolor');
+const { TESTING_MODE } = require('../../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -18,73 +19,84 @@ module.exports = {
                 .setDescription(`The amount of ${CURRENCY_NAME} to bet.`)
                 .setRequired(true)),
     async execute(interaction) {
-        const user = interaction.user;
-        const option = interaction.options.getString('bet');
-        const stats = `${user.id}.stats.craps`;
+        if (TESTING_MODE) {        
+            const user = interaction.user;
+            const option = interaction.options.getString('bet');
+            const stats = `${user.id}.stats.craps`;
 
-        let bet = Number(await parseBet(option, user.id));
-        const dbUser = await db.get(user.id);
+            let bet = Number(await parseBet(option, user.id));
+            const dbUser = await db.get(user.id);
 
-        if (db.get(stats) === undefined) {
-            db.set(stats, { wins: 0, losses: 0, royals: 0, biggestWin: 0, biggestLoss: 0 });
-        }
+            if (db.get(stats) === undefined) {
+                db.set(stats, { wins: 0, losses: 0, royals: 0, biggestWin: 0, biggestLoss: 0 });
+            }
 
-        logger.info(`User ${user.username}#${user.discriminator} is playing craps with a bet of ${bet} ${CURRENCY_NAME}.`);
+            logger.info(`User ${user.username}#${user.discriminator} is playing craps with a bet of ${bet} ${CURRENCY_NAME}.`);
 
-        const error_embed = new EmbedBuilder()
-            .setAuthor({name: interaction.user.username+"#"+interaction.user.discriminator, iconURL: interaction.user.displayAvatarURL({dynamic: true})})
-            .setColor(0xFF0000)
-            .setTitle('Error')
-            .setFooter({text: `Meme Cultist | Version ${require('../../package.json').version}`, iconURL: interaction.client.user.displayAvatarURL({dynamic: true})})
+            const error_embed = new EmbedBuilder()
+                .setAuthor({name: interaction.user.username+"#"+interaction.user.discriminator, iconURL: interaction.user.displayAvatarURL({dynamic: true})})
+                .setColor(0xFF0000)
+                .setTitle('Error')
+                .setFooter({text: `Meme Cultist | Version ${require('../../package.json').version}`, iconURL: interaction.client.user.displayAvatarURL({dynamic: true})})
+                .setTimestamp();
+    
+            if (!dbUser) {
+                await addNewDBUser(user.id);
+                error_embed.setDescription(`You don't have an account! Please try using the \`daily\` command and then try again.`);
+                return interaction.reply({ embeds: [error_embed], ephemeral: true });
+            }
+            if (bet > dbUser.balance) {
+                error_embed.setDescription(`You don't have enough ${CURRENCY_NAME}!`);
+                return interaction.reply({ embeds: [error_embed], ephemeral: true });
+            }
+            if (bet < 1) {
+                error_embed.setDescription(`You must bet at least 1 ${CURRENCY_NAME}!`);
+                return interaction.reply({ embeds: [error_embed], ephemeral: true });
+            }
+            if (bet % 1 !== 0) {
+                error_embed.setDescription(`You must bet in whole numbers!`);
+                return interaction.reply({ embeds: [error_embed], ephemeral: true });
+            }
+            
+            let msg = await interaction.deferReply();
+            const dice = [await roll(6, 1), await roll(6, 1)];
+
+            const diceImage = await drawDice(dice[0], dice[1]);
+
+            const embed = new EmbedBuilder()
+            .setAuthor({ name: user.username, iconURL: user.displayAvatarURL({ dynamic: true }) })
+            .setColor(randomHexColor())
+            .setImage(`attachment://roll.png`)
+            .setFooter({ text: `Bet: ${bet} ${CURRENCY_NAME} | Meme Cultist | Version ${require('../../package.json').version}`, iconURL: interaction.client.user.displayAvatarURL({ dynamic: true }) })
             .setTimestamp();
- 
-        if (!dbUser) {
-            await addNewDBUser(user.id);
-            error_embed.setDescription(`You don't have an account! Please try using the \`daily\` command and then try again.`);
-            return interaction.reply({ embeds: [error_embed], ephemeral: true });
-        }
-        if (bet > dbUser.balance) {
-            error_embed.setDescription(`You don't have enough ${CURRENCY_NAME}!`);
-            return interaction.reply({ embeds: [error_embed], ephemeral: true });
-        }
-        if (bet < 1) {
-            error_embed.setDescription(`You must bet at least 1 ${CURRENCY_NAME}!`);
-            return interaction.reply({ embeds: [error_embed], ephemeral: true });
-        }
-        if (bet % 1 !== 0) {
-            error_embed.setDescription(`You must bet in whole numbers!`);
-            return interaction.reply({ embeds: [error_embed], ephemeral: true });
-        }
-        
-        let msg = await interaction.deferReply();
-        const dice = [await roll(6, 1), await roll(6, 1)];
 
-        const diceImage = await drawDice(dice[0], dice[1]);
-
-        const embed = new EmbedBuilder()
-        .setAuthor({ name: user.username, iconURL: user.displayAvatarURL({ dynamic: true }) })
-        .setColor(randomHexColor())
-        .setImage(`attachment://roll.png`)
-        .setFooter({ text: `Bet: ${bet} ${CURRENCY_NAME} | Meme Cultist | Version ${require('../../package.json').version}`, iconURL: interaction.client.user.displayAvatarURL({ dynamic: true }) })
-        .setTimestamp();
-
-        logger.debug(dice.join(', '));
-        
-        if (dice[0] === 1 && dice[1] === 1) {
-            embed.setDescription(`You rolled a **2** and lost **${bet}** ${CURRENCY_NAME}!`);
-            return await interaction.editReply({ embeds: [embed], files: [diceImage], fetchReply: true });
-        } else if (dice[0] === 6 && dice[1] === 6) {
-            embed.setDescription(`You rolled a **12** and won **${bet * 7}** ${CURRENCY_NAME}!`);
-            return await interaction.editReply({ embeds: [embed], files: [diceImage], fetchReply: true });
-        } else if (dice[0] + dice[1] === 7 || dice[0] + dice[1] === 11) {
-            embed.setDescription(`You rolled a **${dice[0] + dice[1]}** and won **${bet}** ${CURRENCY_NAME}!`);
-            return await interaction.editReply({ embeds: [embed], files: [diceImage], fetchReply: true });
-        } else if (dice[0] + dice[1] === 2 || dice[0] + dice[1] === 3 || dice[0] + dice[1] === 12) {
-            embed.setDescription(`You rolled a **${dice[0] + dice[1]}** and lost **${bet}** ${CURRENCY_NAME}!`);
-            return await interaction.editReply({ embeds: [embed], files: [diceImage], fetchReply: true });
+            logger.debug(dice.join(', '));
+            
+            if (dice[0] === 1 && dice[1] === 1) {
+                embed.setDescription(`You rolled a **2** and lost **${bet}** ${CURRENCY_NAME}!`);
+                return await interaction.editReply({ embeds: [embed], files: [diceImage], fetchReply: true });
+            } else if (dice[0] === 6 && dice[1] === 6) {
+                embed.setDescription(`You rolled a **12** and won **${bet * 7}** ${CURRENCY_NAME}!`);
+                return await interaction.editReply({ embeds: [embed], files: [diceImage], fetchReply: true });
+            } else if (dice[0] + dice[1] === 7 || dice[0] + dice[1] === 11) {
+                embed.setDescription(`You rolled a **${dice[0] + dice[1]}** and won **${bet}** ${CURRENCY_NAME}!`);
+                return await interaction.editReply({ embeds: [embed], files: [diceImage], fetchReply: true });
+            } else if (dice[0] + dice[1] === 2 || dice[0] + dice[1] === 3 || dice[0] + dice[1] === 12) {
+                embed.setDescription(`You rolled a **${dice[0] + dice[1]}** and lost **${bet}** ${CURRENCY_NAME}!`);
+                return await interaction.editReply({ embeds: [embed], files: [diceImage], fetchReply: true });
+            } else {
+                embed.setDescription(`You rolled a **${dice[0] + dice[1]}**. Roll again to win or lose!`);
+                return await interaction.editReply({ embeds: [embed], files: [diceImage], fetchReply: true });
+            }
         } else {
-            embed.setDescription(`You rolled a **${dice[0] + dice[1]}**. Roll again to win or lose!`);
-            return await interaction.editReply({ embeds: [embed], files: [diceImage], fetchReply: true });
+            const error_embed = new EmbedBuilder()
+                .setAuthor({name: interaction.user.username+"#"+interaction.user.discriminator, iconURL: interaction.user.displayAvatarURL({dynamic: true})})
+                .setColor(0xFF0000)
+                .setTitle('Error')
+                .setFooter({text: `Meme Cultist | Version ${require('../../package.json').version}`, iconURL: interaction.client.user.displayAvatarURL({dynamic: true})})
+                .setTimestamp();
+            error_embed.setDescription(`This command is currently disabled.`);
+            return interaction.reply({ embeds: [error_embed], ephemeral: true });
         }
     }
 };
