8b99d4f2cce814f860ea2e5c4ea1948de23e2ed3
brendanpayne <brendan@compu-medic.net>
2026-04-02 17:05:00 -0400

    feat: add progressive jackpot command to check current jackpot amount

---
+++ commands/currency/jackpot.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { getJackpot, MIN_BET } = require('../../utils/jackpot');
+const { CURRENCY_NAME } = require('../../config.js');
+const { randomHexColor } = require('../../utils/randomcolor');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('jackpot')
+        .setDescription('Check the current progressive jackpot amount.'),
+    async execute(interaction) {
+        const jackpot = await getJackpot();
+
+        const embed = new EmbedBuilder()
+            .setTitle('🎰 Progressive Jackpot')
+            .setColor(randomHexColor())
+            .setDescription(`**Current Jackpot:** ${jackpot.amount.toLocaleString()} ${CURRENCY_NAME}`)
+            .addFields(
+                { name: 'Minimum Bet to Qualify', value: `${MIN_BET.toLocaleString()} ${CURRENCY_NAME}`, inline: true },
+                { name: 'Games', value: 'Slots, Poker', inline: true }
+            )
+            .setFooter({
+                text: `${interaction.client.user.username} | Version ${require('../../package.json').version}`,
+                iconURL: interaction.client.user.displayAvatarURL({ dynamic: true })
+            })
+            .setTimestamp();
+
+        // Show last winner if there is one
+        if (jackpot.lastWinner) {
+            const lastWonDate = new Date(jackpot.lastWon);
+            embed.addFields({
+                name: 'Last Winner',
+                value: `**${jackpot.lastWinner.name}** won ${jackpot.amount.toLocaleString()} ${CURRENCY_NAME}\n<t:${Math.floor(lastWonDate.getTime() / 1000)}:R>`,
+                inline: false
+            });
+        }
+
+        await interaction.reply({ embeds: [embed] });
+    },
+};
